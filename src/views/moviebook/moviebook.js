19082c102da31a572946f4d6693781b1033f638b
Utkp5 <utkp200@gmail.com>
2022-09-21 02:36:52 +0530

    frontend

--- src/views/moviebook/moviebook.js
+++ src/views/moviebook/moviebook.js
@@ -50,6 +50,16 @@ function Moviebook() {
         console.log(error);
       })
   }
+  // const handlePayment = async () => {
+	// 	try {
+	// 		const orderUrl = "http://localhost:5000/api/Payment/orders";
+	// 		const { data } = await axios.post(orderUrl, { amount: book.price });
+	// 		console.log(data);
+	// 		initPayment(data.data);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// };
 
   return (
     <div className='return_div_moviebook'>
@@ -61,6 +71,7 @@ function Moviebook() {
                 <p className="movie-book-p" >{Moviebook.movieName}</p>
                 <p className="movie-book-p1" >{Moviebook.language} . {Moviebook.genres} . {Moviebook.year}</p>
                 <p className="movie-book-p2" >{Moviebook.time}</p>
+                <p className="movie-book-p2" >Rs.99 Only</p>
                 <div className='movie-book-button-div'>
                 <button className='movie-book-button' onClick={() => {
                   handleBooking();
